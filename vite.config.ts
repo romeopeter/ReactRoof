c09e933110351ab8d605a6e59544bccc6085bc33
Romeo <romeomeshack34@gmail.com>
2026-01-03 04:31:50 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,41 @@ import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      rollupTypes: true,
-      tsconfigPath: './tsconfig.app.json'
-    })
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/roof/index.ts'),
-      name: 'ReactRoof',
-      fileName: 'react-roof',
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'jsxRuntime'
+export default defineConfig(({ mode }) => {
+  if (mode === 'demo') {
+    return {
+      base: '/react-roof/',
+      plugins: [react()],
+      build: {
+        outDir: 'dist-demo',
+      }
+    }
+  }
+
+  return {
+    plugins: [
+      react(),
+      dts({
+        rollupTypes: true,
+        tsconfigPath: './tsconfig.app.json'
+      })
+    ],
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/roof/index.ts'),
+        name: 'ReactRoof',
+        fileName: 'react-roof',
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom', 'react/jsx-runtime'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+            'react/jsx-runtime': 'jsxRuntime'
+          },
         },
       },
-    },
-  },
+    }
+  }
 })
