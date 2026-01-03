1ab2c00df5942c6bb1c42759ad3bc58b763adba9
Romeo <romeomeshack34@gmail.com>
2026-01-03 03:23:32 +0100

    Configure for library publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json'
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/roof/index.ts'),
+      name: 'ReactRoof',
+      fileName: 'react-roof',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime'
+        },
+      },
+    },
+  },
 })
